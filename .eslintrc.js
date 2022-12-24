0d65c3286f5f19ea4d7f16c0effe6505c71f1bcb
plasticine9750 <975036719@qq.com>
2022-12-24 13:16:32 +0800

    feat: integrate with eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/** @type { import('eslint').ESLint.ConfigData } */
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+  },
+}
